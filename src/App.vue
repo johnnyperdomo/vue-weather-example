42f8342c4d80a399d9735e7d35e086dd4bc71147
Johnny Perdomo <johnnyperdomo7@gmail.com>
2021-12-12 18:06:59 -0500

    finish project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,107 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <main>
+            <div class="search-box">
+                <input
+                    type="text"
+                    class="search-box"
+                    placeholder="search..."
+                    v-model="query"
+                    @keypress="fetchWeather"
+                />
+            </div>
+
+            <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+                <div class="location-box">
+                    <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+                    <div class="date">Monday 20 January 2020</div>
+                </div>
+
+                <div class="weather-box">
+                    <div class="temp">{{ Math.round(weather.main.temp) }} degrees</div>
+                    <div class="weather">{{ weather.weather[0].main }}</div>
+                </div>
+            </div>
+        </main>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "app",
+    data() {
+        return {
+            api_key: "a5766192f003e678e1f33722d141ccbb",
+            url_base: "https://api.openweathermap.org/data/2.5/",
+            query: "",
+            weather: {},
+        };
+    },
+    methods: {
+        //only fetch if user clicks enter
+        fetchWeather(e) {
+            if (e.key == "Enter") {
+                fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+                    .then((res) => {
+                        console.log(res);
+                        return res.json();
+                    })
+                    .then(this.setResults);
+            }
+        },
+        setResults(results) {
+            this.weather = results;
+        },
+        dateBuilder() {},
+    },
+};
 </script>
 
-<style>
+<style scoped>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    font-family: monospace;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    background-image: url("./assets/cold-bg.jpeg");
+    background-size: cover;
+    background-position: bottom;
+    transition: 0.4s;
+}
+
+main {
+    min-height: 100vh;
+    padding: 25px;
+}
+
+.search-box {
+    width: 100%;
+    margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+    display: block;
+    width: 100%;
+    padding: 15px;
+
+    color: black;
+    font-size: 20px;
+}
+
+.location-box .location {
+    color: black;
+    font-size: 32px;
+    font-weight: 500;
+    text-align: center;
+}
+
+.weather-wrap {
+    text-align: center;
 }
 </style>
